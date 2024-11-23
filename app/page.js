6bb670a3562e0050ad5b4c8099d0fccb7a4c4789
hasanasadov <hasanaliasadov@gmail.com>
2024-11-24 01:25:47 +0400

    mobile bug fix

--- app/page.js
+++ app/page.js
@@ -4,15 +4,14 @@ import GoogleMap from "@/components/shared/GoogleMap";
 
 export default function Home() {
   return (
-    <div >
-      <div className="flex justify-between bg-gray-200  p-8 w-full">
-          <SideBarLeft />
-          <div className="flex-grow rounded-xl overflow-hidden mx-6 " >
-
+    <div>
+      <div className="flex justify-between flex-col md:flex-row bg-gray-200  p-8 w-full">
+        <SideBarLeft />
+        <div className="flex-grow rounded-xl overflow-hidden mx-6 ">
           <GoogleMap />
-          </div>
-          <SideBarRight />
         </div>
+        <SideBarRight />
+      </div>
     </div>
   );
 }
