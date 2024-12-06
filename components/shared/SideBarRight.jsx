97e85fead8f38a33aa7d95dbb6bcc3a3b14d499f
hasanasadov <hasanaliasadov@gmail.com>
2024-12-06 19:00:46 +0400

    googlemap demo added

--- components/shared/SideBarRight.jsx
+++ components/shared/SideBarRight.jsx
@@ -3,7 +3,7 @@ import VenuesSection from "./Venues/VenuesSection";
 
 const SideBarRight = () => {
   return (
-    <div className="h-[40vh] md:h-[80vh] md:w-1/4 w-full  rounded-xl border overflow-hidden shadow-lg">
+    <div className="h-[40vh] md:h-[80vh] md:w-1/4 w-full  rounded-xl border overflow-hidden">
       <div className="flex flex-col h-full">
         <div className="flex items-center justify-center h-16 w-full border-b-2  ">
           <h1 className="text-2xl font-extrabold ">Venues</h1>
--- services/events/index.js
+++ services/events/index.js
@@ -31,6 +31,29 @@ export const addFavoriteEvent = async (id) => {
   const data = await res.json();
   return data;
 };
+export const createEvent = async (inputData) => {
+  try {
+    const response = await fetch("http://localhost:8000/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
 export const removeFavoriteEvent = async (id) => {
   const res = await fetch(`${BASE_URL}/favorite-events/${id}`, {
