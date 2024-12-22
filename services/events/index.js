bf6e08aaec01e8d861ccdc30d05bce6982ce1d87
hasanasadov <hasanaliasadov@gmail.com>
2024-12-22 20:16:54 +0400

    adding some services

--- services/events/index.js
+++ services/events/index.js
@@ -62,3 +62,104 @@ export const removeFavoriteEvent = async (id) => {
   const data = await res.json();
   return data;
 };
+
+export async function getFavoriteEvents() {
+  try {
+    const response = await fetch("http://localhost:8000/events/favorites", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function getEvent(event_id) {
+  try {
+    const response = await fetch("http://localhost:8000/events/" + event_id);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function getEventComments(event_id) {
+  try {
+    const response = await fetch(
+      "http://localhost:8000/events/" + event_id + "/comment"
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function getEventComment(comment_id) {
+  try {
+    const response = await fetch(
+      "http://localhost:8000/events/comment/" + comment_id
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function createEvent(inputData) {
+  try {
+    const response = await fetch("http://localhost:8000/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function createEventLike(inputData) {
+  try {
+    const response = await fetch("http://localhost:8000/events/like", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({
+        event: inputData.event,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
--- services/users/index.js
+++ services/users/index.js
@@ -111,3 +111,92 @@ export async function handleResetPassword(values) {
     return null;
   }
 }
+
+// export async function google_auth(payload) {
+//   let data = {
+//     id: payload["sub"],
+//     email: payload["email"],
+//     picture: payload["picture"],
+//     provider: "google",
+//   };
+//   if (payload["given_name"]) {
+//     data["first_name"] = payload["given_name"];
+//   } else {
+//     data["first_name"] = "";
+//   }
+
+//   if (payload["family_name"]) {
+//     data["last_name"] = payload["family_name"];
+//   } else {
+//     data["last_name"] = "";
+//   }
+
+//   if (payload["name"]) {
+//     data["display_name"] = payload["name"];
+//   } else {
+//     data["display_name"] = "";
+//   }
+
+//   return data;
+// }
+
+// export async function handleCredentialResponse(response) {
+//   const errorMessageContainer = document.getElementById("error-message");
+//   var tokens = response.credential.split(".");
+//   var payload = JSON.parse(atob(tokens[1]));
+
+//   try {
+//     const googleAuthData = await google_auth(payload);
+//     const response = await sendGoogleAuthRequest(googleAuthData);
+//     const responseData = await response.json();
+
+//     if (response.ok) {
+//       const token = responseData.access_token;
+//       if (token) {
+//         localStorage.setItem("access_token", token);
+//         console.log("Token saved to localStorage");
+
+//         errorMessageContainer.style.display = "block";
+//         errorMessageContainer.style.textAlign = "center";
+//         errorMessageContainer.textContent = "Successful Entry";
+//         errorMessageContainer.style.color = "green";
+
+//         setTimeout(() => {
+//           window.location.href = "http://127.0.0.1:3000/CLIENT";
+//         }, 3000);
+//       } else {
+//         console.error("No token received in response");
+//         errorMessageContainer.style.display = "block";
+//         errorMessageContainer.style.textAlign = "center";
+//         errorMessageContainer.textContent =
+//           "No token received. Please try again.";
+//         errorMessageContainer.style.color = "red";
+//       }
+//     } else {
+//       console.error("Failed to authenticate:", responseData);
+//       errorMessageContainer.style.display = "block";
+//       errorMessageContainer.style.textAlign = "center";
+//       errorMessageContainer.textContent = responseData.detail;
+//       errorMessageContainer.style.color = "red";
+//     }
+//   } catch (error) {
+//     console.error("Error during login request:", error);
+//     errorMessageContainer.style.display = "block";
+//     errorMessageContainer.textContent =
+//       "An error occurred. Please try again later.";
+//     errorMessageContainer.style.color = "red";
+//   }
+// }
+
+// window.onload = function () {
+//   google.accounts.id.initialize({
+//     client_id:
+//       "1054962718888-1q0f5m7hbnqjkvo241mj7jbhh5ljmji2.apps.googleusercontent.com",
+//     callback: handleCredentialResponse,
+//   });
+//   google.accounts.id.renderButton(
+//     document.getElementById("buttonDiv"),
+//     { theme: "outline", size: "large" } // customization attributes
+//   );
+//   google.accounts.id.prompt(); // also display the One Tap dialog
+// };
