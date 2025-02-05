c568c134166a09d3403bfe0245c74eb0263493a6
hasanasadov <hasanaliasadov@gmail.com>
2025-02-05 20:28:54 +0400

    sidebar bug fix

--- components/shared/SideBarRight.jsx
+++ components/shared/SideBarRight.jsx
@@ -3,13 +3,11 @@ import VenuesSection from "./Venues/VenuesSection";
 
 const SideBarRight = () => {
   return (
-    <div className="bg-white h-[40vh] md:h-[85vh] md:w-1/4 w-full  rounded-lg border overflow-hidden">
-      <div className="flex flex-col h-full">
-        <div className="flex items-center justify-center h-16 w-full ">
-          <h1 className="text-2xl font-extrabold ">Venues</h1>
-        </div>
-        <VenuesSection />
+    <div className="bg-white h-[40vh] md:h-[85vh] md:w-1/4 w-full  overflow-hidden flex justify-between  border rounded-lg flex-col">
+      <div className="text-center p-4 w-full  h-fit text-2xl font-extrabold text-black">
+        Venues
       </div>
+      <VenuesSection />
     </div>
   );
 };
